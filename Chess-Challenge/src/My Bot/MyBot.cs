db35c412bcc5cb899a1e3fc36b62ae7699b30386
LukeDaSkywalker <lukas.harfich@gmail.com>
2023-12-02 16:03:09 +0100

    evaluation refactor + kinda minimax

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,16 +1,15 @@
 ﻿using ChessChallenge.API;
+using Raylib_cs;
 using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
         int[] values = { 0, 1, 3, 3, 5, 9, 0 };
-        PieceList[] pieceList = board.GetAllPieceLists();
-        float eval = 0;
-        float highestEval = 99;
-        Move bestMove = moves[0];
+        decimal eval = 0;
         int[] pawnTable = {0,  0,  0,  0,  0,  0,  0,  0,
 50, 50, 50, 50, 50, 50, 50, 50,
 10, 10, 20, 30, 30, 20, 10, 10,
@@ -19,40 +18,134 @@ public class MyBot : IChessBot
  5, -5,-10,  0,  0,-10, -5,  5,
  5, 10, 10,-20,-20, 10, 10,  5,
  0,  0,  0,  0,  0,  0,  0,  0};
+        int[] knightTable = {-50,-40,-30,-30,-30,-30,-40,-50,
+-40,-20,  0,  0,  0,  0,-20,-40,
+-30,  0, 10, 15, 15, 10,  0,-30,
+-30,  5, 15, 20, 20, 15,  5,-30,
+-30,  0, 15, 20, 20, 15,  0,-30,
+-30,  5, 10, 15, 15, 10,  5,-30,
+-40,-20,  0,  5,  5,  0,-20,-40,
+-50,-40,-30,-30,-30,-30,-40,-50};
+        int[] bishopTable = {-20,-10,-10,-10,-10,-10,-10,-20,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-10,  0,  5, 10, 10,  5,  0,-10,
+-10,  5,  5, 10, 10,  5,  5,-10,
+-10,  0, 10, 10, 10, 10,  0,-10,
+-10, 10, 10, 10, 10, 10, 10,-10,
+-10,  5,  0,  0,  0,  0,  5,-10,
+-20,-10,-10,-10,-10,-10,-10,-20};
+        int[] rookTable = {0,  0,  0,  0,  0,  0,  0,  0,
+  5, 10, 10, 10, 10, 10, 10,  5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+  0,  0,  0,  5,  5,  0,  0,  0};
+        int[] queenTable = {-20,-10,-10, -5, -5,-10,-10,-20,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-10,  0,  5,  5,  5,  5,  0,-10,
+ -5,  0,  5,  5,  5,  5,  0, -5,
+  0,  0,  5,  5,  5,  5,  0, -5,
+-10,  5,  5,  5,  5,  5,  0,-10,
+-10,  0,  5,  0,  0,  0,  0,-10,
+-20,-10,-10, -5, -5,-10,-10,-20};
+        int[] kingTable = {-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-20,-30,-30,-40,-40,-30,-30,-20,
+-10,-20,-20,-20,-20,-20,-20,-10,
+ 20, 20,  0,  0,  0,  0, 20, 20,
+ 20, 30, 10,  0,  0, 10, 30, 20};
+        float best = 0;
 
-
-        if (board.IsWhiteToMove)
+        Move minimax(Board board, int depth, Move theMove)
         {
-            highestEval = -99;
-        }
-
-        foreach (Move move in moves)
-        {
-            switch (board.IsWhiteToMove)
+            if (depth == 0)
             {
-                case true:
-                    if (highestEval < evaluation(board, move))
-                    {
-                        highestEval = evaluation(board, move);
-                        bestMove = move;
-                    }
-                    break;
-                case false:
-                    if (evaluation(board, move) < highestEval)
-                    {
-                        highestEval = evaluation(board, move);
-                        bestMove = move;
-                    }
-                    break;
+                return ;
             }
+            if (board.IsWhiteToMove)
+            {
+                float highestEval = -1000;
+                foreach(Move move in board.GetLegalMoves())
+                {
+                    board.MakeMove(move);
+                    float val = minimax(board, depth - 1);
+                    if(val > best)
+                    {
+                        best = val;
+                    }
+                }
+            }
+            else
+            {
+                float highestEval = 1000;
+            }
+            return;
         }
 
-        float evaluation(Board board, Move moveToEvaluate)
+
+        float evaluation(Board board)
         {
-            board.MakeMove(moveToEvaluate);
             eval = 0;
+            PieceList[] pieceList = board.GetAllPieceLists();
             foreach (PieceList pieceList2 in pieceList)
             {
+                for (int i = 0; i < pieceList2.Count; i++)
+                {
+                    if (pieceList2.IsWhitePieceList)
+                    {
+                        eval += values[(int)pieceList2.GetPiece(i).PieceType];
+                        switch ((int)pieceList2.GetPiece(i).PieceType)
+                        {
+                            case 1:
+                                eval += pawnTable[63 - pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 2:
+                                eval += knightTable[63 - pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 3:
+                                eval += bishopTable[63 - pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 4:
+                                eval += rookTable[63 - pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 5:
+                                eval += queenTable[63 - pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 6:
+                                eval += kingTable[63 - pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        eval -= values[(int)pieceList2.GetPiece(i).PieceType];
+                        switch ((int)pieceList2.GetPiece(i).PieceType)
+                        {
+                            case 1:
+                                eval -= pawnTable[pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 2:
+                                eval -= knightTable[pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 3:
+                                eval -= bishopTable[pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 4:
+                                eval -= rookTable[pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 5:
+                                eval -= queenTable[pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                            case 6:
+                                eval -= kingTable[pieceList2.GetPiece(i).Square.Index] / 100m;
+                                break;
+                        }
+                    }
+                }
                 /*if (pieceList2.IsWhitePieceList)
                 {
                     eval += pieceList2.Count * values[(int)pieceList2.TypeOfPieceInList];
@@ -61,11 +154,10 @@ public class MyBot : IChessBot
                 {
                     eval -= pieceList2.Count * values[(int)pieceList2.TypeOfPieceInList];
                 }*/
-                
+
             }
-            board.UndoMove(moveToEvaluate);
-            return eval;
+            return (float)eval;
         }
-        return bestMove;
+        return;
     }
 }
