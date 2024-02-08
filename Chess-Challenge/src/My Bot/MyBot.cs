eb6d9d482b34030f8c24d9f92dcbd734f477ebfe
LukeDaSkywalker <lukas.harfich@gmail.com>
2024-02-08 21:49:38 +0100

    check extensions

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -12,12 +12,10 @@ public class MyBot : IChessBot
         public sbyte depth;
         public byte flag;
     }
-    public Move Think(Board board, Timer timer)
-    {
-        int[] mg_value = { 0, 82, 337, 365, 477, 1025, 0 };
-        int[] eg_value = { 0, 94, 281, 297, 512, 936, 0 };
-        int[] pieceValue = { 0, 100, 300, 300, 500, 900, 0 };
-        int[] mg_pawn_table = {
+    readonly int[] mg_value = { 0, 82, 337, 365, 477, 1025, 0 };
+    readonly int[] eg_value = { 0, 94, 281, 297, 512, 936, 0 };
+    readonly int[] pieceValue = { 0, 100, 300, 300, 500, 900, 0 };
+    readonly int[] mg_pawn_table = {
       0,   0,   0,   0,   0,   0,  0,   0,
     -11,  34, 126,  68,  95,  61, 134,  98,
     -20,  25,  56,  65,  31,  26,   7,  -6,
@@ -27,7 +25,7 @@ public class MyBot : IChessBot
     -22,  38,  24, -15, -23, -20,  -1, -35,
       0,   0,   0,   0,   0,   0,  0,   0
 };
-        int[] eg_pawn_table = {
+    readonly int[] eg_pawn_table = {
       0,   0,   0,   0,   0,   0,   0,   0,
 187, 165, 132, 147, 134, 158, 173, 178,
  84,  82,  53,  56,  67,  85, 100,  94,
@@ -37,7 +35,7 @@ public class MyBot : IChessBot
  -7,   2,   0,  13,  10,   8,   8,  13,
   0,   0,   0,   0,   0,   0,   0,   0
 };
-        int[] mg_knight_table = {
+    readonly int[] mg_knight_table = {
     -107, -15, -97,  61, -49, -34, -89, -167,
  -17,   7,  62,  23,  36,  72, -41, -73,
   44,  73, 129,  84,  65,  37,  60, -47,
@@ -47,7 +45,7 @@ public class MyBot : IChessBot
  -19, -14,  18,  -1,  -3, -12, -53, -29,
  -23, -19, -28, -17, -33, -58, -21, -105
 };
-        int[] eg_knight_table = {
+    readonly int[] eg_knight_table = {
     -99, -63, -27, -31, -28, -13, -38, -58,
  -52, -24, -25,  -9,  -2, -25,  -8, -25,
  -41, -19,  -9,  -1,   9,  10, -20, -24,
@@ -57,7 +55,7 @@ public class MyBot : IChessBot
  -44, -23, -20,  -2,  -5, -10, -20, -42,
  -64, -50, -18, -22, -15, -23, -51, -29
 };
-        int[] mg_bishop_table = {
+    readonly int[] mg_bishop_table = {
     -8,   7, -42, -25, -37, -82,   4, -29,
  -47,  18,  59,  30, -13, -18,  16, -26,
   -2,  37,  50,  35,  40,  43,  37, -16,
@@ -67,7 +65,7 @@ public class MyBot : IChessBot
    1,  33,  21,   7,   0,  16,  15,   4,
  -21, -39, -12, -13, -21, -14,  -3, -33
 };
-        int[] eg_bishop_table = {
+    readonly int[] eg_bishop_table = {
     -24, -17,  -9,  -7,  -9,  -7, -21, -14,
 -14,  -4, -13,  -3, -12,   7,  -4,  -8,
   4,   0,   6,  -2,  -1,   0,  -8,   2,
@@ -77,7 +75,7 @@ public class MyBot : IChessBot
 -27, -15,  -9,   4,  -1,  -7, -18, -14,
 -17,  -5, -16,  -9,  -5, -23,  -9, -23
 };
-        int[] mg_rook_table = {
+    readonly int[] mg_rook_table = {
      43,  31,   9,  63,  51,  32,  42,  32,
  44,  26,  67,  80,  62,  58,  32,  27,
  16,  61,  45,  17,  36,  26,  19,  -5,
@@ -87,7 +85,7 @@ public class MyBot : IChessBot
 -71,  -6,  11,  -1,  -9, -20, -16, -44,
 -26, -37,   7,  16,  17,   1, -13, -19
 };
-        int[] eg_rook_table = {
+    readonly int[] eg_rook_table = {
     5,   8,  12,  12,  15,  18,  10,  13,
   3,   8,   3,  -3,  11,  13,  13,  11,
  -3,  -5,   4,   7,   7,   7,   7,   7,
@@ -97,7 +95,7 @@ public class MyBot : IChessBot
  -3, -11,  -9,  -9,   2,   0,  -6,  -6,
 -20,   4, -13,  -5,  -1,   3,   2,  -9
 };
-        int[] mg_queen_table = {
+    readonly int[] mg_queen_table = {
     45,  43,  44,  59,  12,  29,   0, -28,
  54,  28,  57, -16,   1,  -5, -39, -24,
  57,  47,  56,  29,   8,   7, -17, -13,
@@ -107,7 +105,7 @@ public class MyBot : IChessBot
   1,  -3,  15,   8,   2,  11,  -8, -35,
 -50, -31, -25, -15,  10,  -9, -18,  -1
 };
-        int[] eg_queen_table = {
+    readonly int[] eg_queen_table = {
      20,  10,  19,  27,  27,  22,  22,  -9,
   0,  30,  25,  58,  41,  32,  20, -17,
   9,  19,  35,  47,  49,   9,   6, -20,
@@ -117,7 +115,7 @@ public class MyBot : IChessBot
 -32, -36, -23, -16, -16, -30, -23, -22,
 -41, -20, -32,  -5, -43, -22, -28, -33
 };
-        int[] mg_king_table = {
+    readonly int[] mg_king_table = {
 13,   2, -34, -56, -15,  16,  23, -65,
 -29, -38,  -4,  -8,  -7, -20,  -1,  29,
 -22,  22,   6, -20, -16,   2,  24,  -9,
@@ -127,7 +125,7 @@ public class MyBot : IChessBot
   8,   9, -16, -43, -64,  -8,   7,   1,
  14,  24, -28,   8, -54,  12,  36, -15
 };
-        int[] eg_king_table = {
+    readonly int[] eg_king_table = {
     -17,   4,  15,  11, -18, -18, -35, -74,
  11,  23,  38,  17,  17,  14,  17, -12,
  13,  44,  45,  20,  15,  23,  17,  10,
@@ -137,7 +135,7 @@ public class MyBot : IChessBot
 -17,  -5,   4,  14,  13,   4, -11, -27,
 -43, -24, -14, -28, -11, -21, -34, -53
 };
-        int[] black_mg_pawn_table = {
+    readonly int[] black_mg_pawn_table = {
       0,   0,   0,   0,   0,   0,  0,   0,
      98, 134,  61,  95,  68, 126, 34, -11,
      -6,   7,  26,  31,  65,  56, 25, -20,
@@ -147,7 +145,7 @@ public class MyBot : IChessBot
     -35,  -1, -20, -23, -15,  24, 38, -22,
       0,   0,   0,   0,   0,   0,  0,   0,
 };
-        int[] black_eg_pawn_table = {
+    readonly int[] black_eg_pawn_table = {
       0,   0,   0,   0,   0,   0,   0,   0,
     178, 173, 158, 134, 147, 132, 165, 187,
      94, 100,  85,  67,  56,  53,  82,  84,
@@ -157,7 +155,7 @@ public class MyBot : IChessBot
      13,   8,   8,  10,  13,   0,   2,  -7,
       0,   0,   0,   0,   0,   0,   0,   0,
 };
-        int[] black_mg_knight_table = {
+    readonly int[] black_mg_knight_table = {
     -167, -89, -34, -49,  61, -97, -15, -107,
      -73, -41,  72,  36,  23,  62,   7,  -17,
      -47,  60,  37,  65,  84, 129,  73,   44,
@@ -167,7 +165,7 @@ public class MyBot : IChessBot
      -29, -53, -12,  -3,  -1,  18, -14,  -19,
     -105, -21, -58, -33, -17, -28, -19,  -23,
 };
-        int[] black_eg_knight_table = {
+    readonly int[] black_eg_knight_table = {
     -58, -38, -13, -28, -31, -27, -63, -99,
     -25,  -8, -25,  -2,  -9, -25, -24, -52,
     -24, -20,  10,   9,  -1,  -9, -19, -41,
@@ -177,7 +175,7 @@ public class MyBot : IChessBot
     -42, -20, -10,  -5,  -2, -20, -23, -44,
     -29, -51, -23, -15, -22, -18, -50, -64,
 };
-        int[] black_mg_bishop_table = {
+    readonly int[] black_mg_bishop_table = {
     -29,   4, -82, -37, -25, -42,   7,  -8,
     -26,  16, -18, -13,  30,  59,  18, -47,
     -16,  37,  43,  40,  35,  50,  37,  -2,
@@ -187,7 +185,7 @@ public class MyBot : IChessBot
       4,  15,  16,   0,   7,  21,  33,   1,
     -33,  -3, -14, -21, -13, -12, -39, -21,
 };
-        int[] black_eg_bishop_table = {
+    readonly int[] black_eg_bishop_table = {
     -14, -21, -11,  -8, -7,  -9, -17, -24,
      -8,  -4,   7, -12, -3, -13,  -4, -14,
       2,  -8,   0,  -1, -2,   6,   0,   4,
@@ -197,7 +195,7 @@ public class MyBot : IChessBot
     -14, -18,  -7,  -1,  4,  -9, -15, -27,
     -23,  -9, -23,  -5, -9, -16,  -5, -17,
 };
-        int[] black_mg_rook_table = {
+    readonly int[] black_mg_rook_table = {
      32,  42,  32,  51, 63,  9,  31,  43,
      27,  32,  58,  62, 80, 67,  26,  44,
      -5,  19,  26,  36, 17, 45,  61,  16,
@@ -207,7 +205,7 @@ public class MyBot : IChessBot
     -44, -16, -20,  -9, -1, 11,  -6, -71,
     -19, -13,   1,  17, 16,  7, -37, -26,
 };
-        int[] black_eg_rook_table = {
+    readonly int[] black_eg_rook_table = {
     13, 10, 18, 15, 12,  12,   8,   5,
     11, 13, 13, 11, -3,   3,   8,   3,
      7,  7,  7,  5,  4,  -3,  -5,  -3,
@@ -217,7 +215,7 @@ public class MyBot : IChessBot
     -6, -6,  0,  2, -9,  -9, -11,  -3,
     -9,  2,  3, -1, -5, -13,   4, -20,
 };
-        int[] black_mg_queen_table = {
+    readonly int[] black_mg_queen_table = {
     -28,   0,  29,  12,  59,  44,  43,  45,
     -24, -39,  -5,   1, -16,  57,  28,  54,
     -13, -17,   7,   8,  29,  56,  47,  57,
@@ -227,7 +225,7 @@ public class MyBot : IChessBot
     -35,  -8,  11,   2,   8,  15,  -3,   1,
      -1, -18,  -9,  10, -15, -25, -31, -50,
 };
-        int[] black_eg_queen_table = {
+    readonly int[] black_eg_queen_table = {
      -9,  22,  22,  27,  27,  19,  10,  20,
     -17,  20,  32,  41,  58,  25,  30,   0,
     -20,   6,   9,  49,  47,  35,  19,   9,
@@ -237,7 +235,7 @@ public class MyBot : IChessBot
     -22, -23, -30, -16, -16, -23, -36, -32,
     -33, -28, -22, -43,  -5, -32, -20, -41,
 };
-        int[] black_mg_king_table = {
+    readonly int[] black_mg_king_table = {
     -65,  23,  16, -15, -56, -34,   2,  13,
      29,  -1, -20,  -7,  -8,  -4, -38, -29,
      -9,  24,   2, -16, -20,   6,  22, -22,
@@ -247,7 +245,7 @@ public class MyBot : IChessBot
       1,   7,  -8, -64, -43, -16,   9,   8,
     -15,  36,  12, -54,   8, -28,  24,  14,
 };
-        int[] black_eg_king_table = {
+    readonly int[] black_eg_king_table = {
     -74, -35, -18, -18, -11,  15,   4, -17,
     -12,  17,  14,  17,  17,  38,  23,  11,
      10,  17,  23,  15,  20,  45,  44,  13,
@@ -257,11 +255,14 @@ public class MyBot : IChessBot
     -27, -11,   4,  13,  14,   4,  -5, -17,
     -53, -34, -21, -11, -28, -14, -24, -43
 };
+
+    public Move Think(Board board, Timer timer)
+    {
         int[][] white_mg_table = { Array.Empty<int>(), mg_pawn_table, mg_knight_table, mg_bishop_table, mg_rook_table, mg_queen_table, mg_king_table };
         int[][] black_mg_table = { Array.Empty<int>(), black_mg_pawn_table, black_mg_knight_table, black_mg_bishop_table, black_mg_rook_table, black_mg_queen_table, black_mg_king_table };
         int[][] white_eg_table = { Array.Empty<int>(), eg_pawn_table, eg_knight_table, eg_bishop_table, eg_rook_table, eg_queen_table, eg_king_table };
         int[][] black_eg_table = { Array.Empty<int>(), black_eg_pawn_table, black_eg_knight_table, black_eg_bishop_table, black_eg_rook_table, black_eg_queen_table, black_eg_king_table };
-        /*int currentPhase = 24;
+        int currentPhase = 24;
         currentPhase -= BitboardHelper.GetNumberOfSetBits(board.GetPieceBitboard(PieceType.Knight, true));
         currentPhase -= BitboardHelper.GetNumberOfSetBits(board.GetPieceBitboard(PieceType.Bishop, true));
         currentPhase -= BitboardHelper.GetNumberOfSetBits(board.GetPieceBitboard(PieceType.Rook, true)) * 2;
@@ -271,13 +272,13 @@ public class MyBot : IChessBot
         currentPhase -= BitboardHelper.GetNumberOfSetBits(board.GetPieceBitboard(PieceType.Rook, false)) * 2;
         currentPhase -= BitboardHelper.GetNumberOfSetBits(board.GetPieceBitboard(PieceType.Queen, false)) * 4;
         currentPhase = (currentPhase * 256 + 12) / 24;
-        Console.WriteLine(currentPhase);*/
         Move bestRootMove = new();
         int depthdepth = 1;
         int score = new();
         // alpha beta pruning
-        int alphabeta(Board board, int depth, int alpha, int beta, bool root)
+        int alphabeta(Board board, int depth, int alpha, int beta, bool root, int extension)
         {
+            int extension2 = 0;
             int startingAlpha = alpha;
             int bestEval = -100000;
             ref Transposition tp = ref tpt[board.ZobristKey & 0x3FFFFFF];
@@ -317,7 +318,7 @@ public class MyBot : IChessBot
                         Console.WriteLine(bestRootMove);
                         Convert.ToUInt32(-1);
                     }
-                    score = evaluation(board, depth);
+                    score = evaluation(board, depth - extension);
                 }
                 else if (board.IsDraw())
                 {
@@ -325,19 +326,24 @@ public class MyBot : IChessBot
                 }
                 else
                 {
+                    if (board.IsInCheck() && extension < 16)
+                    {
+                        extension2++;
+                    }
                     if (bSearchPv)
                     {
-                        score = -alphabeta(board, depth - 1, -beta, -alpha, false);
+                        score = -alphabeta(board, depth - 1 + extension2, -beta, -alpha, false, extension + extension2);
                     }
                     else
                     {
-                        score = -alphabeta(board, depth - 1, -alpha - 1, -alpha, false);
+                        score = -alphabeta(board, depth - 1 + extension2, -alpha - 1, -alpha, false, extension + extension2);
                         if (score > alpha)
                         {
-                            score = -alphabeta(board, depth - 1, -beta, -alpha, false);
+                            score = -alphabeta(board, depth - 1 + extension2, -beta, -alpha, false, extension + extension2);
                         }
                     }
                 }
+                extension2 = 0;
                 board.UndoMove(move);
                 if (score > bestEval) bestEval = score;
                 if (score >= beta)
@@ -348,7 +354,7 @@ public class MyBot : IChessBot
                 {
                     alpha = score;
                     bSearchPv = false;
-                    if (depth == depthdepth)
+                    if (root)
                     {
                         bestRootMove = move;
                         //Console.WriteLine(bestRootMove);
@@ -369,7 +375,6 @@ public class MyBot : IChessBot
             tp.depth = (sbyte)depth;
             return alpha;
         }
-
         int evaluation(Board board, int depth)
         {
             int eval = 0;
@@ -430,22 +435,23 @@ public class MyBot : IChessBot
 
         }
         int currentEval = 0;
-        Console.WriteLine("V1.14");
+        Console.WriteLine("V1.16");
         try
         {
             for (; ; depthdepth++)
             {
                 //iterative deepening
-                currentEval = alphabeta(board, depthdepth, -100000, 100000, true);
+                currentEval = alphabeta(board, depthdepth, -100000, 100000, true, 0);
                 if (!board.IsWhiteToMove)
                     currentEval *= -1;
-                Console.WriteLine(depthdepth);
+                //Console.WriteLine(depthdepth);
+                //Console.WriteLine(currentEval / 100f);
             }
         }
         catch
         {
             Console.WriteLine(currentEval / 100f);
-            //Console.WriteLine(depthdepth);
+            Console.WriteLine(depthdepth);
             return bestRootMove;
         }
     }
